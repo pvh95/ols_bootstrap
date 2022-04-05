4d12293110f554d0fd54d47d5d1e105d37a1906c
Viet Hung Pham <phamvh95@gmail.com>
2022-04-05 17:43:11 +0200

    Fully vectorized residual bs

--- ols_bootstrap/residual.py
+++ ols_bootstrap/residual.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pandas as pd
-import copy
 
 from ols_bootstrap.auxillary.linreg import LR
 from ols_bootstrap.pairs import PairsBootstrap
 from prettytable import PrettyTable, ALL
-from collections import OrderedDict
 
 
 class ResidualBootstrap(PairsBootstrap):
@@ -14,7 +12,7 @@ class ResidualBootstrap(PairsBootstrap):
         self._bootstrap_type = "Residual Bootstrap"
 
     def _bootstrap(self):
-        self._indep_vars_bs_param_dict = self._init_bs_vars()
+        self._indep_vars_bs_param = np.zeros((len(self._indep_varname), self._iter))
 
         for i in range(self._iter):
             Y_boot = np.zeros(self._sample_size)
@@ -27,7 +25,5 @@ class ResidualBootstrap(PairsBootstrap):
 
             ols_model = LR(Y_boot, self._X)
             ols_model.fit()
-            ols_param_values = ols_model.params
 
-            for (key, values) in zip(self._indep_vars_bs_param_dict, ols_param_values):
-                self._indep_vars_bs_param_dict[key][i] = values
+            self._indep_vars_bs_param[:, i] = ols_model.params
