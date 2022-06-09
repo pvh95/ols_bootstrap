6265f593a58423042f92919629a9aa7d414aef56
Viet Hung Pham <phamvh95@gmail.com>
2022-06-09 12:18:29 +0200

    Adding residual scaling options in HCE-s

--- ols_bootstrap/auxillary/std_error.py
+++ ols_bootstrap/auxillary/std_error.py
@@ -6,12 +6,12 @@ import numpy as np
 class HC0_1:
     def __init__(self, X, residual):
         self._X = X
+        self._residual = residual
 
         pinv_XtX = np.linalg.pinv(self._X.T @ self._X)
-        diag_mtx_HC0 = np.diag(
-            residual ** 2
-        )  # equivalent to residual * residual, which is an element-wise operation
-        self._cov_HC0_HC1 = pinv_XtX @ self._X.T @ diag_mtx_HC0 @ self._X @ pinv_XtX
+        self._cov_HC0_HC1 = (
+            pinv_XtX @ self._X.T @ np.diag(self._residual ** 2) @ self._X @ pinv_XtX
+        )
 
     @property
     def HC0_se(self):
@@ -21,23 +21,23 @@ class HC0_1:
 
     @property
     def HC1_se(self):
-        hc1_se = np.sqrt(
-            np.diag(
-                self._X.shape[0]
-                / (self._X.shape[0] - self._X.shape[1])
-                * self._cov_HC0_HC1
-            )
-        )
+        self._dof_coeff = self._X.shape[0] / (self._X.shape[0] - self._X.shape[1])
+        hc1_se = np.sqrt(self._dof_coeff * np.diag(self._cov_HC0_HC1))
 
         return hc1_se
+
+    @property
+    def HC1_scaled_resid(self):
+        hc1_resid = np.sqrt(self._dof_coeff) * self._residual
+
+        return hc1_resid
 
 
 class HC2_5:
     def __init__(self, X, residual):
         self._X = X
-        self._residual_square_arr = (
-            residual ** 2
-        )  # equivalent to residual * residual, which is an element-wise operation
+        self._residual = residual
+
         self._pinv_XtX = np.linalg.pinv(self._X.T @ self._X)
         self._H_diag = np.diag(self._X @ self._pinv_XtX @ self._X.T)
 
@@ -46,34 +46,54 @@ class HC2_5:
 
         return hccm
 
+    ##########################
+
     @property
     def HC2_se(self):
-        diag_HC2 = np.diag(self._residual_square_arr / (1 - self._H_diag))
-        cov_HC2 = self._HCCM(diag_HC2)
+        self._hc2_resid = self._residual / np.sqrt(1 - self._H_diag)
+        cov_HC2 = self._HCCM(np.diag(self._hc2_resid ** 2))
 
         hc2_se = np.sqrt(np.diag(cov_HC2))
 
         return hc2_se
 
     @property
+    def HC2_scaled_resid(self):
+        return self._hc2_resid
+
+    ##########################
+
+    @property
     def HC3_se(self):
-        diag_HC3 = np.diag(self._residual_square_arr / (1 - self._H_diag) ** 2)
-        cov_HC3 = self._HCCM(diag_HC3)
+        self._hc3_resid = self._residual / (1 - self._H_diag)
+        cov_HC3 = self._HCCM(np.diag(self._hc3_resid ** 2))
 
         hc3_se = np.sqrt(np.diag(cov_HC3))
 
         return hc3_se
 
     @property
+    def HC3_scaled_resid(self):
+        return self._hc3_resid
+
+    ######################
+
+    @property
     def HC4_se(self):
         delta = np.minimum(4, self._X.shape[0] / self._X.shape[1] * self._H_diag)
 
-        diag_HC4 = np.diag(self._residual_square_arr / (1 - self._H_diag) ** delta)
-        cov_HC4 = self._HCCM(diag_HC4)
+        self._hc4_resid = self._residual / np.sqrt((1 - self._H_diag) ** delta)
+        cov_HC4 = self._HCCM(np.diag(self._hc4_resid ** 2))
 
         hc4_se = np.sqrt(np.diag(cov_HC4))
 
         return hc4_se
+
+    @property
+    def HC4_scaled_resid(self):
+        return self._hc4_resid
+
+    ######################
 
     @property
     def HC4m_se(self):
@@ -83,12 +103,18 @@ class HC2_5:
             1, self._X.shape[0] / self._X.shape[1] * self._H_diag
         ) + np.minimum(1.5, self._X.shape[0] / self._X.shape[1] * self._H_diag)
 
-        diag_HC4m = np.diag(self._residual_square_arr / (1 - self._H_diag) ** delta)
-        cov_HC4m = self._HCCM(diag_HC4m)
+        self._hc4m_resid = self._residual / np.sqrt((1 - self._H_diag) ** delta)
+        cov_HC4m = self._HCCM(np.diag(self._hc4m_resid ** 2))
 
         hc4m_se = np.sqrt(np.diag(cov_HC4m))
 
         return hc4m_se
+
+    @property
+    def HC4m_scaled_resid(self):
+        return self._hc4m_resid
+
+    ######################
 
     @property
     def HC5_se(self):
@@ -103,12 +129,16 @@ class HC2_5:
             aux_max_hc5, self._X.shape[0] / self._X.shape[1] * self._H_diag
         )
 
-        diag_HC5 = np.diag(self._residual_square_arr / (1 - self._H_diag) ** delta)
-        cov_HC5 = self._HCCM(diag_HC5)
+        self._hc5_resid = self._residual / np.sqrt((1 - self._H_diag) ** delta)
+        cov_HC5 = self._HCCM(np.diag(self._hc5_resid ** 2))
 
         hc5_se = np.sqrt(np.diag(cov_HC5))
 
         return hc5_se
+
+    @property
+    def HC5_scaled_resid(self):
+        return self._hc5_resid
 
 
 ### Homoscedastic Standard Error Calculation Function
@@ -116,13 +146,21 @@ def homoscedastic_se(X, ssr):
     const_varience = ssr / (X.shape[0] - X.shape[1])
 
     cov_mtx_params = const_varience * np.linalg.pinv(X.T @ X)
-    bse = np.sqrt(np.diag(cov_mtx_params))
+    homo_se = np.sqrt(np.diag(cov_mtx_params))
 
-    return bse
+    return homo_se
 
 
 ### Calculatign the appropriate standard error
-def se_calculation(X, se_type, residual, ssr):
+def se_calculation(
+    X,
+    se_type,
+    residual,
+    ssr,
+    scale_resid=True,
+):
+    hc_resid = residual
+
     if se_type == "constant":
         se = homoscedastic_se(X, ssr)
 
@@ -134,24 +172,42 @@ def se_calculation(X, se_type, residual, ssr):
         hce_basic = HC0_1(X, residual)
         se = hce_basic.HC1_se
 
+        if scale_resid:
+            hc_resid = hce_basic.HC1_scaled_resid
+
     elif se_type == "hc2":
         hce_weighted = HC2_5(X, residual)
         se = hce_weighted.HC2_se
+
+        if scale_resid:
+            hc_resid = hce_weighted.HC2_scaled_resid
 
     elif se_type == "hc3":
         hce_weighted = HC2_5(X, residual)
         se = hce_weighted.HC3_se
 
+        if scale_resid:
+            hc_resid = hce_weighted.HC3_scaled_resid
+
     elif se_type == "hc4":
         hce_weighted = HC2_5(X, residual)
         se = hce_weighted.HC4_se
+
+        if scale_resid:
+            hc_resid = hce_weighted.HC4_scaled_resid
 
     elif se_type == "hc4m":
         hce_weighted = HC2_5(X, residual)
         se = hce_weighted.HC4m_se
 
+        if scale_resid:
+            hc_resid = hce_weighted.HC4m_scaled_resid
+
     elif se_type == "hc5":
         hce_weighted = HC2_5(X, residual)
         se = hce_weighted.HC5_se
 
-    return se
+        if scale_resid:
+            hc_resid = hce_weighted.HC5_scaled_resid
+
+    return se, hc_resid
