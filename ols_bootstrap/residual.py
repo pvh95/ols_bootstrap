afaafb8fda5bfff36537777f48d4ac0282cde02a
Viet Hung Pham <phamvh95@gmail.com>
2022-06-09 12:22:29 +0200

    Adding the residual scaling option for _calc_se

--- ols_bootstrap/residual.py
+++ ols_bootstrap/residual.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ols_bootstrap.auxillary.linreg import LR
 from ols_bootstrap.pairs import PairsBootstrap
+from ols_bootstrap.auxillary.std_error import se_calculation
 
 
 class ResidualBootstrap(PairsBootstrap):
@@ -14,13 +15,25 @@ class ResidualBootstrap(PairsBootstrap):
         ci_type="bc",
         fit_intercept=True,
         seed=None,
+        scale_resid=True,
     ):
+
+        self._scale_resid = scale_resid
         super().__init__(Y, X, reps, se_type, ci, ci_type, fit_intercept, seed)
         self._bootstrap_type = "Residual Bootstrap"
 
+    def _calc_se(self):
+        self._orig_se, self._orig_hc_resid = se_calculation(
+            self._X,
+            self._se_type,
+            self._orig_resid,
+            self._orig_ssr,
+            scale_resid=self._scale_resid,
+        )
+
     def _bootstrap(self):
         boot_residuals_mtx = self._rng.choice(
-            self._orig_resid, (self._reps, self._sample_size), replace=True
+            self._orig_hc_resid, (self._reps, self._sample_size), replace=True
         )
 
         Y_boot_mtx = self._orig_pred_train + boot_residuals_mtx
